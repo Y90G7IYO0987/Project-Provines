f05d75155ddc478d17509db1198aba16b03473c7
Ats Profi Code <atsprofi427@gmail.com>
2026-09-01 17:23:52 +0300

    Add - setting input actions to Player.cs from GameInput.cs

--- Assets/Code/GameInput.cs
+++ Assets/Code/GameInput.cs
@@ -4,9 +4,17 @@ public class GameInput : MonoBehaviour
 {
     public InputActions InputActions { get; private set; }
 
+    [SerializeField] private PlayerData playerData;
+
+    private Player _player;
+
     private void Awake()
     {
         InputActions = new InputActions();
         InputActions.Enable();
+
+        _player = playerData.Prefab.GetComponent<Player>();
+        Debug.Log($"Player GameObject - {_player}");
+        _player.SetInputActions(InputActions);
     }
 }
