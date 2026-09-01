c251b96d5b9f1fff7defc30d40f433d133a5fa37
Ats Profi Code <atsprofi427@gmail.com>
2026-09-01 17:25:07 +0300

    Add new player controls

--- Assets/PlayerControls/InputActions.cs
+++ Assets/PlayerControls/InputActions.cs
@@ -71,6 +71,15 @@ public partial class @InputActions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""SettingsInteraction"",
+                    ""type"": ""Button"",
+                    ""id"": ""b1f4320c-8d4d-4525-a852-fb3877fdd0b1"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -194,6 +203,17 @@ public partial class @InputActions: IInputActionCollection2, IDisposable
                     ""action"": ""Run"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""7e927ffa-e0c0-4832-8ec6-1b6c3e6e6c11"",
+                    ""path"": ""<Keyboard>/o"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""SettingsInteraction"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -207,6 +227,7 @@ public partial class @InputActions: IInputActionCollection2, IDisposable
         m_Player_MenuMovement = m_Player.FindAction("MenuMovement", throwIfNotFound: true);
         m_Player_MenuInteraction = m_Player.FindAction("MenuInteraction", throwIfNotFound: true);
         m_Player_Run = m_Player.FindAction("Run", throwIfNotFound: true);
+        m_Player_SettingsInteraction = m_Player.FindAction("SettingsInteraction", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -273,6 +294,7 @@ public partial class @InputActions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_MenuMovement;
     private readonly InputAction m_Player_MenuInteraction;
     private readonly InputAction m_Player_Run;
+    private readonly InputAction m_Player_SettingsInteraction;
     public struct PlayerActions
     {
         private @InputActions m_Wrapper;
@@ -282,6 +304,7 @@ public partial class @InputActions: IInputActionCollection2, IDisposable
         public InputAction @MenuMovement => m_Wrapper.m_Player_MenuMovement;
         public InputAction @MenuInteraction => m_Wrapper.m_Player_MenuInteraction;
         public InputAction @Run => m_Wrapper.m_Player_Run;
+        public InputAction @SettingsInteraction => m_Wrapper.m_Player_SettingsInteraction;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -306,6 +329,9 @@ public partial class @InputActions: IInputActionCollection2, IDisposable
             @Run.started += instance.OnRun;
             @Run.performed += instance.OnRun;
             @Run.canceled += instance.OnRun;
+            @SettingsInteraction.started += instance.OnSettingsInteraction;
+            @SettingsInteraction.performed += instance.OnSettingsInteraction;
+            @SettingsInteraction.canceled += instance.OnSettingsInteraction;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -325,6 +351,9 @@ public partial class @InputActions: IInputActionCollection2, IDisposable
             @Run.started -= instance.OnRun;
             @Run.performed -= instance.OnRun;
             @Run.canceled -= instance.OnRun;
+            @SettingsInteraction.started -= instance.OnSettingsInteraction;
+            @SettingsInteraction.performed -= instance.OnSettingsInteraction;
+            @SettingsInteraction.canceled -= instance.OnSettingsInteraction;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -349,5 +378,6 @@ public partial class @InputActions: IInputActionCollection2, IDisposable
         void OnMenuMovement(InputAction.CallbackContext context);
         void OnMenuInteraction(InputAction.CallbackContext context);
         void OnRun(InputAction.CallbackContext context);
+        void OnSettingsInteraction(InputAction.CallbackContext context);
     }
 }
